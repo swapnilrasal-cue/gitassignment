227b58040497f3f8bcd46cca538177063a3a699d
swapnilrasal6677 <swapnil.rasal@cuelogic.com>
2019-12-25 18:48:26 +0530

    commit 4 : added multiplication method in calculation.java

--- calculation.java
+++ calculation.java
@@ -6,11 +6,17 @@ class calculation
   {
    int a=5,b=10,c;
    c=addition(a,b);
+   e=multiplication(a,b);
    System.out.println(" Addition of two numbers is : "+c);
   }
 
   static int addition(int x,int y)
   {
    return x+y;
+  }
+
+  static int multiplication(int x,int y)
+  {
+   return x*y;
   }
 }
