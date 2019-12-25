fde76444fbacc9966c00a155978016aec46ee91a
swapnilrasal6677 <swapnil.rasal@cuelogic.com>
2019-12-25 17:29:24 +0530

    commit 2 : Added  addition method in calulation.java file

---
+++ calculation.java
@@ -0,0 +1,16 @@
+# gitassignment
+
+class calculation
+{
+  public static void main(String[] arg)
+  {
+   int a=5,b=10,c;
+   c=addition(a,b);
+   System.out.println(" Addition of two numbers is : "+c);
+  }
+
+  static int addition(int x,int y)
+  {
+   return x+y;
+  }
+}
