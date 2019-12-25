eaf5ef3c27a775ac2652ee06c3063429bb80a09e
swapnilrasal6677 <swapnil.rasal@cuelogic.com>
2019-12-25 20:34:21 +0530

    commit 16: added division method in calculation.java

--- calculation.java
+++ calculation.java
@@ -4,11 +4,21 @@ class calculation
 {
   public static void main(String[] arg)
   {
-   int a=5,b=10,c;
+   int a=5,b=10,c,f;
    c=addition(a,b);
    System.out.println(" Addition of two numbers is : "+c);
+   
+   f=division(a,b);
+   System.out.println("Division of two number is :"+f);
+  
+  }
+  
+  static int division(int x,int y)
+  {
+   return x/y;
   }
 
+  
   static int addition(int x,int y)
   {
    return x+y;
